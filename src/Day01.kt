2872bb6671132c09afb40f4b755921e7de685ca0
atuladhar1 <atuladhar1@gmail.com>
2024-12-01 12:40:00 -0500

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,25 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import kotlin.math.abs
 
-    fun part2(input: List<String>): Int {
-        return input.size
+fun main() {
+    fun totalDistance(list1: List<Int>, list2: List<Int>): Int {
+        return list1.sorted().zip(list2.sorted()).map { (item1, item2) ->
+            abs(item1 - item2)
+                .also { println("$item1 $item2 $it") }
+        }.sum()
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    totalDistance(
+        list1 = listOf(3, 4, 2, 1, 3, 3),
+        list2 = listOf(4, 3, 5, 3, 9, 3)
+    ).also {
+        println(it)
+        check(it == 11)
+    }
 }
